46672a02adc2485d1ea4e0570df1a73f8b812720
Анастасия <nastya.nigm@gmail.com>
2025-06-29 15:13:10 +0000

    Add ReDoc documentation

--- app_main/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-from .models import User
-
-main_bp = Blueprint('main', __name__)
-
-@main_bp.route('/')
-def index():
-    return jsonify({"message": "Main service OK"})
